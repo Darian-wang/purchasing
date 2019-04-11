e9bb1f9f578fb3043192b10007f20d7cea27090e
Your Name <you@example.com>
2019-04-11 17:49:19 +0800

    add goods add function

--- goods/urls.py
+++ goods/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, search
+from .views import index, search, add
 
 
 urlpatterns = [
     path('', index, name='index'),
-    path('search/', search, name='search')
+    path('search/', search, name='search'),
+    path('add/', add, name='add')
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, reverse
-from .models import Goods
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from .models import Goods, Categories
+from purchasing.settings import BASE_DIR
 # Create your views here.
 
 
@@ -13,3 +14,32 @@ def search(request):
         goods = Goods.objects.filter(goods_name__contains=goods_name)
         return render(request, 'search.html', context={'goods': goods})
     return redirect(reverse('index'))
+
+
+def add(request):
+    if request.method == 'GET':
+        categories = Categories.objects.all()
+        print('#'*30)
+        print(categories)
+        return render(request, 'add.html', context={'categories': categories})
+    else:
+        goods_name = request.POST.get('goods_name')
+        goods_price_in = request.POST.get('goods_price_in')
+        goods_price_out = request.POST.get('goods_price_out')
+        goods_desc = request.POST.get('goods_desc')
+        goods_address = request.POST.get('goods_address')
+        category_id = request.POST.get('goods_categories')
+        # goods_photo = request.POST.get('goods_photo')
+        import os
+        file_obj = request.FILES.get('goods_photo')
+        print(type(file_obj))
+        print(file_obj.name)
+        with open(os.path.join(BASE_DIR, 'goods', 'static', file_obj.name), 'wb') as f:
+            for chunk in file_obj.chunks():
+                f.write(chunk)
+        goods_category = Categories.objects.get(pk=category_id)
+        goods = Goods(goods_name=goods_name, goods_price_in=goods_price_in, goods_price_out=goods_price_out,
+                      goods_desc=goods_desc, goods_address=goods_address, goods_category=goods_category,
+                      goods_photo=file_obj.name)
+        goods.save()
+        return HttpResponse('添加商品成功')
