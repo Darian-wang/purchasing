cc8b8479134216bc708ad01fa2bfa871288fb142
Your Name <you@example.com>
2019-04-11 15:08:11 +0800

    search goods according to goods_name

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Categories(models.Model):
+    categories_name = models.CharField(max_length=100)
+
+
+class Goods(models.Model):
+    goods_name = models.CharField(max_length=200)
+    goods_price_in = models.FloatField()
+    goods_price_out = models.FloatField()
+    goods_desc = models.CharField(max_length=10000)
+    goods_address = models.CharField(max_length=1000)
+    goods_category = models.ForeignKey("Categories", on_delete=models.CASCADE)
+    goods_photo = models.CharField(max_length=1000)
+
+
--- goods/views.py
+++ goods/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, reverse
+from .models import Goods
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def search(request):
+    goods_name = request.POST.get('goods_name')
+    if goods_name:
+        goods = Goods.objects.filter(goods_name__contains=goods_name)
+        return render(request, 'search.html', context={'goods': goods})
+    return redirect(reverse('index'))
