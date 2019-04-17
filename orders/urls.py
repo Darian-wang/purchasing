bc459a0414e60cae3b26f55e1b63b6be99721a0f
Your Name <you@example.com>
2019-04-17 13:40:34 +0800

    add is buy

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import add_order, order_list, delete_order, is_buy, order_list_on_bought
+from .views import add_order, delete_order, is_buy, order_list_on_bought
+from .views import OrderListView
 
 app_name = 'orders'
 
 urlpatterns = [
-    path('', order_list, name='order_list'),
+    path('', OrderListView.as_view(), name='order_list'),
     path('add_order/', add_order, name='add_order'),
     path('delete_order/', delete_order, name='delete_order'),
     path('is_buy/', is_buy, name='is_buy'),
--- orders/views.py
+++ orders/views.py
@@ -2,12 +2,51 @@ from django.shortcuts import render
 from goods.models import Goods
 from .models import User, Order
 from django.shortcuts import redirect, reverse
+from django.views.generic import ListView
+from django.core.paginator import Paginator, Page
 import time
 
 
-def order_list(request):
-    orders = Order.objects.all()
-    return render(request, 'order_list.html', context={'orders': orders})
+class OrderListView(ListView):
+    model = Order
+    template_name = 'order_list.html'
+    context_object_name = 'orders'
+    paginate_by = 10
+    page_kwarg = 'page'
+    ordering = '-create_time'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context.get('paginator')
+        page_obj = context.get('page_obj')
+        pagination_data = self.get_pagination_data(paginator, page_obj)
+        context.update(pagination_data)
+        print('#'*10)
+        print(type(paginator.object_list))
+        return context
+
+    def get_pagination_data(self, paginator, page_obj, around_count=2):
+        current_page = page_obj.number
+        pages_count = paginator.num_pages
+        left_has_more = False
+        right_has_more = False
+
+        if current_page - around_count <= 2:
+            left_pages = range(1, current_page)
+        else:
+            left_has_more = True
+            left_pages = range(current_page - around_count, current_page)
+
+        if current_page + around_count >= pages_count - 1:
+            right_pages = range(current_page + 1, pages_count + 1)
+        else:
+            right_has_more = True
+            right_pages = range(current_page + 1, current_page + around_count + 1)
+
+        return {'left_pages': left_pages,
+                'right_pages': right_pages,
+                'left_has_more': left_has_more,
+                'right_has_more': right_has_more, }
 
 
 def delete_order(request):
@@ -15,6 +54,11 @@ def delete_order(request):
     orders = Order.objects.get(pk=order_id)
     orders.delete()
     return redirect(reverse('orders:order_list'))
+
+
+def search_order_by_username(request):
+    username = request.POST.get('username')
+    pass
 
 
 def order_list_on_bought(request):
@@ -25,7 +69,12 @@ def order_list_on_bought(request):
 def is_buy(request):
     order_id = request.GET.get('order_id')
     orders = Order.objects.get(pk=order_id)
-    orders.is_buy = True
+
+    buy = request.POST.get('is_buy')
+    if buy == 'yes':
+        orders.is_buy = False
+    else:
+        orders.is_buy = True
     orders.save()
     return redirect(reverse('orders:order_list'))
 
@@ -40,7 +89,6 @@ def add_order(request):
         user_address = request.POST.get('user_address')
         user = User(user_name=user_name, user_phone=user_phone,
                     user_address=user_address)
-        print(user)
         user.save()
         goods_id = request.POST.get('goods_id')
         goods = Goods.objects.get(pk=goods_id)
